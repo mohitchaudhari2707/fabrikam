5540ae0c85a9b02d2c47894ab6d68ebe4f0e1256
mohitchaudhari2707 <mohitchaudhari2707@gmail.com>
2022-02-21 18:04:38 +0530

    fileUploaded

---
+++ server.js
@@ -0,0 +1,40 @@
+/*var count=34;
+count++;
+console.log("Server is running");
+console.log(count);
+
+
+//CTL + SHIFT + C   
+
+*/
+
+var express =require("express");  //lib-----Node Module
+var app=express();  // will create app object from express package
+
+var onAboutUs=function(req, res){
+    res.send("Chief Mentor :Ravi Tambade");
+};
+var onDefault=function(req, res){
+    res.send("<h1>Fabrikam</h1>"+
+              "<hr/>"+
+              "<ol>"+
+                    " <li>fabrikam, is a multinational company</li>"+
+                    " <li>It has 27 offices all over the world</li>"+
+                    " <li>Hosting public web site for their potential customers</li>"+
+                    " <li>host intranet application for all employees to access Human Resource, Accounting and Project Management related information.</li>"+
+                    
+                    
+             "</ol>");
+};
+app.get("/",onDefault);  // Request handler functions are registered
+app.get("/aboutus",onAboutUs);  // Request handler functions are registered
+var server=app.listen(8081);
+console.log("Server is running on port 8081");
+// if external packages are required
+// we have to download those packages from internet using  package manager tool
+// Runtime--------tools-------------packages-----------------------config file
+// Java-----------maven--------------.jar files----------------------pom.xml
+// Android--------graddle------------android jar file----------------pom.xml
+// Python---------pip----------------Python libraries----------------
+// .NET ---------Nugget--------------dll files-----------------------package.config
+// NodeJS----------npm --------------download node modules-----------package.json 
